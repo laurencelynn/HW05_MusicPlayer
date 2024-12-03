9700b48249876f2e7a523667924bf99b576adf62
LaurenceLin <laurencelin0614@icloud.com>
2024-12-03 18:29:45 +0800

    Initial Commit

---
+++ HW05_MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW05_MusicPlayer
+//
+//  Created by 林上閔 on 2024/12/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW05_MusicPlayerTests/HW05_MusicPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HW05_MusicPlayerTests.swift
+//  HW05_MusicPlayerTests
+//
+//  Created by 林上閔 on 2024/12/3.
+//
+
+import Testing
+@testable import HW05_MusicPlayer
+
+struct HW05_MusicPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
